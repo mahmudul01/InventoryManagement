40bf15e1c24c77e340cac867fc4348f228794187
mahmudul01 <69353551+mahmudul01@users.noreply.github.com>
2024-12-08 17:10:14 +0600

    Index page

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Customer Page</h5>
+            <p class="card-text">Click the button below to go to customer page</p>
+            <a href="/InventoryManagement/Customer/index.php" class="btn btn-primary">View</a>
+        </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Supplier Page</h5>
+            <p class="card-text">Click the button below to go to supplier page</p>
+            <a href="/InventoryManagement/Supplier/index.php" class="btn btn-primary">View</a>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
